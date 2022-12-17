6cbf7edc42acd990ea1e38307ead986658ad26ee
Bosco Ho <boscojwho@gmail.com>
2022-12-16 16:12:21 -0800

    - ADD: Custom target paths.

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SheetInteraction-SPM",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/SheetInteraction"),
         .testTarget(
             name: "SheetInteractionTests",
-            dependencies: ["SheetInteraction-SPM"]),
+            dependencies: ["SheetInteraction-SPM"],
+            path: "Tests/SheetInteractionTests"),
     ]
 )
