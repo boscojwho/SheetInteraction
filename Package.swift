cc59e6e802c3fcecbba4bcb19609f2acc400bbdb
Bosco Ho <boscojwho@gmail.com>
2022-12-16 16:48:45 -0800

    - CHANGE: Exclude tests.

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
             name: "SheetInteraction",
             dependencies: [],
             path: "SheetInteraction",
-            exclude: ["Example/"]),
+            exclude: ["Tests"]),
         .testTarget(
             name: "SheetInteractionTests",
             dependencies: ["SheetInteraction"]),
