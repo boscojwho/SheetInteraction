af5b8fa8a9a38f1e1f68a61279725439cb5bd52f
Bosco Ho <boscojwho@gmail.com>
2022-12-16 16:20:52 -0800

    - CHANGE: Target.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SheetInteraction",
-            targets: ["SheetInteraction-SPM"]),
+            targets: ["SheetInteraction"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,13 +19,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SheetInteraction-SPM",
+            name: "SheetInteraction",
             dependencies: [],
             path: "Sources/SheetInteraction",
             sources: ["Sources/SheetInteraction"]),
         .testTarget(
             name: "SheetInteractionTests",
-            dependencies: ["SheetInteraction-SPM"],
+            dependencies: ["SheetInteraction"],
             path: "Tests/SheetInteractionTests",
             sources: ["Tests/SheetInteractionTests"]),
     ]
